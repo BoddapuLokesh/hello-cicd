b0d1219677d41d3440dc764bb8be91e9a3e3f99d
Lokesh <129824193+BoddapuLokesh@users.noreply.github.com>
2025-10-31 10:12:47 +0530

    calculator application

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-def greet(name):
-    return f"Hello, {name}!"
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b == 0:
+        print("Division by zero, Invalid Input")
+    return a/b
 
 if __name__ == "__main__":
-    print(greet("Jenkins"))
+    print(add(2,3))
+    print(subtract(4,2))
+    print(multiply(7,2))
+    print(divide(12,6))
--- test_app.py
+++ test_app.py
@@ -1,9 +1,26 @@
 import unittest
-from app import greet
+from app import add, multiply, divide, subtract
 
 class TestApp(unittest.TestCase):
-    def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World!")
+    def test_add(self):
+        self.assertEqual(add(2,3),5)
+        self.assertEqual(add(-1,1),0)
+        self.assertEqual(add(4,2),6)
+    
+    def test_subtract(self):
+        self.assertEqual(subtract(1,1),0)
+        self.assertEqual(subtract(1,1),0)
+        self.assertEqual(subtract(1,1),0)
+    
+    def test_multiply(self):
+        self.assertEqual(multiply(3,1),3)
+        self.assertEqual(multiply(4,2),8)
+        self.assertEqual(multiply(3,3),9)
+
+    def test_divide(self):
+        self.assertEqual(divide(4,2),2)
+        self.assertEqual(divide(3,1),3)
+        self.assertEqual(divide(9,3),3)
 
 if __name__ == '__main__':
     unittest.main()
